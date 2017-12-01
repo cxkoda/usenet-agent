b5d48c649b3d5679ec24b4dfeca048f1cd988fa2
cxkoda <davedoc@huiba.info>
2017-12-01 22:55:36 +0100

    General improvements and introducing robobrowser

--- usenetAgentLib.py
+++ usenetAgentLib.py
@@ -3,13 +3,18 @@ import stem.process
 import os, platform, signal
 import socks, socket
 
-import datetime, time
+from bs4 import BeautifulSoup
 import requests
+
+import datetime, time
 import string
 import random
+
+
+
 from configobj import ConfigObj
 
-class sabConfigurator:
+class sabnzbdHandler:
 	def __init__(self, cfg):
 		self.cfg = cfg
 		cfgSabnzbdPath = os.getenv(self.cfg['sabnzbd'][platform.system()]['start_dir']) + \
@@ -32,22 +37,45 @@ class sabConfigurator:
 		except requests.exceptions.RequestException:
 			pass
 
+class usedIPList:
+	def __init__(self, filePath):
+		self.usedIpsCFG = ConfigObj(filePath)
+		self.usedIps = {}
+
+		for key, value in self.usedIpsCFG.items():
+			self.usedIps[key] = value.split(' ')
+
+	def write(self):
+
+		self.usedIpsCFG.write()
+
+
+
 
 
 class usenetAgent:
 	def __init__(self, cfgPath, cfgHostName, sabHostName):
 		self.cfg = ConfigObj(cfgPath)
-		self.sabCfg = sabConfigurator(self.cfg)
+		self.sabHandler = sabnzbdHandler(self.cfg)
 
 		self.cfgHostName = cfgHostName
 		self.sabHostName = sabHostName
 
 		self.socksPort = 9050
 
+		try:
+			pass
+		except:
+			self.usedIpList = None
+
 	def __enter__(self):
 		return self
 
 	def __exit__(self, exc_type, exc_value, traceback):
+		self.closeTorConnection()
+		self.cfg.write()
+
+	def closeTorConnection(self):
 		if hasattr(self, 'torProcess'):
 			print('Killing Tor Process', self.torProcess.pid)
 			self.torProcess.kill()
@@ -64,9 +92,17 @@ class usenetAgent:
 			init_msg_handler = print_bootstrap_lines)
 		socks.set_default_proxy(socks.SOCKS5, "127.0.0.1", self.socksPort)
 		socket.socket = socks.socksocket
+		self.getIP()
+
+	def getIP(self):
+		self.ip = requests.get('http://icanhazip.com').text.split('\n')[0]
 
 	def testConnection(self):
-		print(requests.get('http://icanhazip.com').text)
+		self.getIP()
+		print(term.format('Request IP: %s' % self.ip, term.Color.GREEN))
+
+	def addUsedIp(self):
+		pass
 
 	def generateRandomString(self, size=10, chars=string.ascii_lowercase + string.digits):
 		return ''.join(random.choice(chars) for _ in range(size))
@@ -90,13 +126,17 @@ class usenetAgent:
 			password = self.generateRandomString()
 		self.HostPassword = password
 
+	def printCredentials(self):
+		print(term.format(self.HostUsername, term.Color.GREEN))
+		print(term.format(self.HostPassword, term.Color.GREEN))
+
 	def writeCfgFiles(self):
 		self.cfg[self.cfgHostName]['username'] = self.HostUsername
 		self.cfg[self.cfgHostName]['password'] = self.HostPassword
 		self.cfg[self.cfgHostName]['generated'] = datetime.datetime.strftime(datetime.datetime.now(), '%a %d-%m-%Y %H:%M:%S')
 		self.cfg[self.cfgHostName]['worked'] = 0
 		self.cfg.write()
-		self.sabCfg.syncSabnzbdEntry(self.cfgHostName, self.sabHostName)
+		self.sabHandler.syncSabnzbdEntry(self.cfgHostName, self.sabHostName)
 
 
 
@@ -105,23 +145,58 @@ class ewekaAgent(usenetAgent):
 		super(ewekaAgent, self).__init__(cfgPath, 'eweka', sabHostName)
 
 	def sendForm(self, mail):
-		response = requests.post('https://www.eweka.nl/en/free_trial/', data={'email': mail})
-		#response = requests.get('https://www.eweka.nl/en/free_trial')
-		print(mail)
-		html_file = open('res.html', 'w+')
-		html_file.write(response.text)
-		html_file.close()
+		print('Random Trial Mail: %s' % mail)
+		#response = requests.post('https://www.eweka.nl/en/free_trial/', data={'email': mail})
+		#parsed = response.text
+		#soup = BeautifulSoup(response.content, 'html.parser')
+
+		browser = RoboBrowser(history=True)
+		browser.open('https://www.eweka.nl/en/free_trial/')
+		form = browser.get_form()
+		form['email'].value = mail
+		browser.submit_form(form)
+		parsed = str(browser.parsed)
+
+		#with open('res.html', 'r') as htmlFile:
+		#	parsed = htmlFile.read()
+
+		if parsed.find('Password') > 0 or parsed.find('password') > 0 or parsed.find('Pass') > 0 or parsed.find('pass') > 0:
+			print('Robo Success')
+			with open('last_success.html', 'w+') as htmlFile:
+				htmlFile.write(parsed)
+
+			start = parsed.find('Password:</i>') + 82
+			len = 6
+			password = parsed[start:start+len]
+			self.setHostPassword(password)
+
+			return True
+
+		return False
+
 
 	def getTrial(self):
-		self.generateRandomMail()
-		self.setHostUsername(self.randomMail)
-		self.sendForm(self.randomMail)
+		n = 0
+		while True:
+			n += 1
+			print(term.format('Trial: %s' % n, term.Color.YELLOW))
+			self.establishTorConnection()
+			self.testConnection()
+			self.generateRandomMail()
+			self.setHostUsername(self.randomMail)
+			if self.sendForm(self.randomMail):
+				break
+			self.closeTorConnection()
+
+		self.printCredentials()
+		self.writeCfgFiles()
+		self.sabHandler.restart()
+		return True
 
 
-
+import re
+from robobrowser import RoboBrowser
 
 if __name__ == '__main__':
-	with ewekaAgent("config.ini") as ua:
-		ua.establishTorConnection()
-		ua.testConnection()
+	with ewekaAgent("config.ini", 'eweka') as ua:
 		ua.getTrial()
